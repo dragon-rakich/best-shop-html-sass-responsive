4c9b49d1ce9045d3cf0149df683ba4abbf2ca3e2
dragon-r <d.rak60@outlook.com>
2025-12-19 16:59:18 +0100

    Found all elements to work on

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-console.log("working?");
+//all ements we will be working on
+const quantityInput = document.querySelector("#quantity");
+const ordersInput = document.querySelector("#orders");
+const dropdownPlaceholder = document.querySelector(".calc__dropdown-placeholder");
+const dropdownList = document.querySelector(".calc__dropdown-list");
+const accountingCheckboxField = document.querySelector(".calc__checkbox-field:first-child");
+const terminalCheckboxField = document.querySelector(".calc__checkbox-field:last-child");
+const outputRows = document.querySelectorAll(".calc__output-row");
+const totalPrice = document.querySelector(".calc__total-price");
