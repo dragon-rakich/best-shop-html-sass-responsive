a49028390bf88e93b7a96443761cc0e522d42b9d
dragon-r <d.rak60@outlook.com>
2025-12-21 19:39:55 +0100

    Added output functions

--- js/app.js
+++ js/app.js
@@ -19,7 +19,6 @@ function inputHandler(e){
 
     const newChar = e.data
     const strInputValue = this.value;
-    const numInputValue = Number(strInputValue);
     if (newChar && (/[+\-\.eE]/.test(newChar))){
         console.log("new char is " + newChar + " character. We make the input value ''!");
         this.value = "";
@@ -28,12 +27,11 @@ function inputHandler(e){
         this.value = "";
     } else if (strInputValue === ""){
         console.log("input is ''. We leave it as is");
-    } else if (e.data === null){
-        console.log("new char is null??")
     } else{
         console.log("input is ok");
     }
-    //updateOutput();
+
+    updateOutput(this);
 }
 
 function clickHandler(e){
@@ -47,12 +45,12 @@ function clickHandler(e){
         selectDropdownOption(this.innerText);
 
         hideDropdown();
-        //updateOutput();
+        updateOutput(this);
     }
 }
 
 function checkboxHandler(e){
-
+    updateOutput(this);
 }
 
 //helper functions to event functions
@@ -72,6 +70,7 @@ function hideDropdown(){
 function selectDropdownOption(option){
     hiddenSelect.value = option;
     dropdownPlaceholder.children[0].innerText = option;
+    dropdownPlaceholder.children[0].style.color = "black";
 }
 
 
@@ -83,4 +82,121 @@ for (let i = 0; i < dropdownList.children.length; i++){dropdownList.children[i].
 accountingCheckboxField.querySelector("#accounting").addEventListener("change", checkboxHandler);
 terminalCheckboxField.querySelector("#terminal").addEventListener("change", checkboxHandler);
 
-//output function
+//output functions
+
+function updateOutput(inputEle){
+    console.log("");
+    console.log("updateOutput");
+
+    const outputRow = getOutputRow(inputEle.id);
+
+    console.log("output row");
+    console.log(outputRow);
+
+    updateOutputRow(outputRow, inputEle);
+    updateTotal();
+}
+
+function getOutputRow(inputEleId){
+    for (let i = 0; i < outputRows.length; i++){
+        if (outputRows[i].id.includes(inputEleId)){
+            return outputRows[i];
+        }
+    }
+    return outputRows[2]; //return the package row - only possibility left
+}
+
+function updateOutputRow(outputRow, inputEle){
+    console.log("update row with id " + outputRow.id);
+
+    if (outputRow.id === "quantity-row" || outputRow.id === "orders-row"){
+        console.log("row is quantity or orders");
+
+        if (inputEle.value === ""){
+            console.log("input value is empty");
+            console.log("we hide the row");
+
+            outputRow.style.display = "none";
+            outputRow.dataset.total = "0";
+
+        } else {
+            console.log("input value isn't empty - will be int number");
+            console.log("we show the row");
+
+            outputRow.style.display = "flex";
+
+            const rowCalc = outputRow.children[1];
+            const rowTotal = outputRow.children[2];
+
+            const numOfItems = Number(inputEle.value);
+            let costOfItem;
+            if (outputRow.id === "quantity-row"){costOfItem = 0.5;} else{costOfItem = 0.25}
+
+            let rowTotalPrice = numOfItems*costOfItem;
+            if (!Number.isInteger(rowTotalPrice)){rowTotalPrice = rowTotalPrice.toFixed(2)};
+
+            rowCalc.innerText = numOfItems + " x $" + costOfItem;
+            rowTotal.innerText = "$" + rowTotalPrice
+
+            outputRow.dataset.total = rowTotalPrice;
+            console.log(typeof(outputRow.dataset.total));
+        }
+    } else if (outputRow.id === "package-row") {
+        console.log("row is package");
+        console.log("we show it");
+        outputRow.style.display = "flex";
+
+        const packageName = outputRow.children[1];
+        const totalPrice = outputRow.children[2];
+
+        let package;
+        let rowTotalPrice
+        if (inputEle.id.includes("basic")){
+            [package, rowTotalPrice] = ["Basic", "0"];
+        } else if (inputEle.id.includes("pro")){
+            [package, rowTotalPrice] = ["Professional", "25"]
+        } else if (inputEle.id.includes("premium")){
+            [package, rowTotalPrice] = ["Premium", "60"]
+        }
+
+        packageName.innerText = package;
+        totalPrice.innerText = "$" + rowTotalPrice;
+        outputRow.dataset.total = rowTotalPrice;
+    } else if (outputRow.id === "accounting-row" || outputRow.id === "terminal-row"){
+        console.log("row is accounting or terminal");
+
+        if (!inputEle.checked){
+            console.log("if checkbox isnt checked, hide row and make its total: $0");
+
+            outputRow.style.display = "none";
+            outputRow.dataset.total = "0";
+        } else {
+            console.log("if checkbox IS checked, show row and determine its total");
+
+            outputRow.style.display = "flex";
+
+            const totalPrice = outputRow.children[1];
+            let rowTotalPrice;
+            if (inputEle.id.includes("accounting")){
+                rowTotalPrice = "35";
+            } else if (inputEle.id.includes("terminal")){
+                rowTotalPrice = "5";
+            }
+            totalPrice.innerText = "$" + rowTotalPrice;
+
+            outputRow.dataset.total = rowTotalPrice;
+        }
+    }
+}
+
+function updateTotal(){
+    console.log("update total!")
+    let totalAmount = 0;
+    for (let i = 0; i < outputRows.length; i++){
+        totalAmount += Number(outputRows[i].dataset.total);
+    }
+
+    if (!Number.isInteger(totalAmount)){totalAmount = totalAmount.toFixed(2)};
+
+    totalPrice.innerText = "$" + totalAmount;
+}
